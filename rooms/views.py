68d78223db586769921622ce855e6687c4a549bb
이혜성 <hyeseong43@gmail.com>
2021-02-06 21:00:47 +0900

    11.6 Handling Exceptions

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from math                  import ceil
-from django.core.paginator import Paginator
-from django.shortcuts      import render
+from django.core.paginator import Paginator, EmptyPage
+from django.shortcuts      import render, redirect
 from rooms                 import models
 
 def all_rooms(request):
@@ -8,13 +8,10 @@ def all_rooms(request):
     
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5) # orphans를 넣게 되면 그 이하로 된 걸 이전페이지에 넣게 된다. 이경우 11페이지에 1개만 있던걸 10페이지에 11개를 가지게함. 
-    rooms = paginator.page(int(page))
-
-    return render(
-        request, 
-        'rooms/homes.html', 
-        context = {
-            'page':rooms,
-        }
-        )
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, 'rooms/home.html', {'page':rooms})
+    except EmptyPage:
+        rooms = paginator.page(1)
+        return redirect('/') #  ?page=12312376878912 과 같이 shiity page 나오면 아예 메인 페이지로 리다이렉트 시켜 url 리셋해버림
 
