64e94a13d70112a656a328df863352aeb8b5b2c2
이혜성 <hyeseong43@gmail.com>
2021-02-06 19:26:45 +0900

    11.2 Pages List Navigation

--- rooms/views.py
+++ rooms/views.py
@@ -5,13 +5,15 @@ from django.shortcuts import render
 from rooms            import models
 
 def all_rooms(request):
-    page      = int(request.GET.get('page',0))
-    page_size = 10
-    limit     = page_size * page    # limit  = 
-    offset    = limit - page_size
+    page      = request.GET.get('page',1)
+    page      = int(page or 1)
+
+    PAGE_SIZE = 10
+    limit     = PAGE_SIZE * page    # limit  = 
+    offset    = limit - PAGE_SIZE
 
     rooms      = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count()/page_size)
+    page_count = ceil(models.Room.objects.count()/PAGE_SIZE)
 
     return render(
         request, 
