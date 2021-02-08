98c3757f174b65a6b3a63d46eeea8ae01d72fed0
이혜성 <hyeseong43@gmail.com>
2021-02-08 13:14:19 +0900

    13.2 Select Choices

--- rooms/views.py
+++ rooms/views.py
@@ -34,6 +34,21 @@ def search(request):
     print((request.GET))
     city = request.GET.get('city','Anywhere')
     city = str.capitalize(city)
+    s_country = request.GET.get('country','KR')
+    room_type_1st_id = models.RoomType.objects.values('pk').first()['pk']
+    print('ㅁㅁㅁㅁ')
+    s_room_type = int(request.GET.get('room_type',room_type_1st_id))
     room_types = models.RoomType.objects.all()
 
-    return render(request, 'rooms/search.html', context={'city':city, 'countries' : countries, 'room_types':room_types })
+    form = {
+        'city':city,
+        's_room_type':s_room_type,
+        's_country':s_country,
+    }
+
+    choices = {
+        'countries':countries,
+        'room_types':room_types,
+    }
+
+    return render(request, 'rooms/search.html', context={**form, **choices})
