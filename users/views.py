e757569cda0a83253cb1d6477cda7eefb819ae17
이혜성 <hyeseong43@gmail.com>
2021-02-10 01:20:49 +0900

    14.3 Validating Password

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ class LoginView(View):
         return render(request, 'users/login.html',context={'form':form})
 
     def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data,'로그인 뷰의 포스트 데이터의 폼.클린데이터 호출')
+        form = forms.LoginForm(request.POST) # forms.py에서 처리한 로직을 form 변수에서 가져옴. 통상 아이디, 비번 정보를 담고 있음
+        if form.is_valid():                  # 데이터가 유효성 검증
+            print(form.clean_data)
         return render(request, 'users/login.html', {"form":form})
