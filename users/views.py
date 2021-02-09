b62798ead59b3620cb4485a19153f24dbc9b3259
이혜성 <hyeseong43@gmail.com>
2021-02-10 00:30:32 +0900

    14.1 Form and CSRF

--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from django.views       import View
 from django.shortcuts   import render, redirect
+from users              import forms
 
 class LoginView(View):
     def get(self, request):
-        return render(request, 'users/login.html')
+        form = forms.LoginForm()
+        return render(request, 'users/login.html',context={'form':form})
 
     def post(self, request):
-        pass
+        form = forms.LoginForm(request.POST)
+        print(form)
