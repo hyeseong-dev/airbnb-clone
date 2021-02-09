a38a09f6061519a4840e9fd7d48863bd7a2cad80
이혜성 <hyeseong43@gmail.com>
2021-02-10 00:13:42 +0900

    14.0 Setting Up!

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,11 @@
-from django.contrib          import admin
-from django.urls             import path,include
-from django.conf             import settings
-from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls    import path,include
+from users          import views
 
 
 app_name = 'users'
 
 urlpatterns = [
+    path('login', views.LoginView.as_view(), name='login')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
