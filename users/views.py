30335cad3bc2e802eda9fc92e7597a9dc412a22a
이혜성 <hyeseong43@gmail.com>
2021-02-11 05:20:42 +0900

    18.0 Getting Authorization Code - 개발자 : 이혜성 - 일시 : 2021.02.11. ~ 2021.02.14 [ ~ : 23:59:59] - 장소 : 재택 - 내용 KAKAO LOGIN 구현   - users/views.py - `kakao_login, kakao_callback, KakaoException` 함수 및 클래스 선언   - `카카오 개발자 설정(앱 생성, redirect_uri, 앱 설정 범위(이름, 사진, 이메일 필수, 선택 옵션) 지정`   - `토큰 받아오는것 까지 완료함`
    - 참고문서 :
      - https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api
    
    - 느낀점:
      - Github API 사용과 큰 차이를 보일 것 같지는 않음.

--- users/views.py
+++ users/views.py
@@ -122,4 +122,21 @@ def github_callback(request):
         # send error message
         return redirect(reverse("users:login"))
 
-        
+def kakao_login(request):
+    client_id = os.environ.get('KAKAO_RESTAPI_KEY')
+    redirect_uri = 'http://127.0.0.1:8000/users/login/kakao/callback'
+    return redirect(
+        f'https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code'
+    )      
+
+class KakaoException(Exception):
+    pass
+
+def kakao_callback(request):
+    try:
+        code = request.GET.get('code')
+        token_request = requests.get(
+             f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id="
+        )
+    except KakaoException:
+        raise KakaoException() 
