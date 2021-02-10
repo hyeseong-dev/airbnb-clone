8b27870c34b387e597ddcc6dc214278ec81259f4
이혜성 <hyeseong43@gmail.com>
2021-02-11 03:26:37 +0900

    17.0 Github Login part1 - 개발자 : 이혜성 - 일시 : 2021.02.10.  [ ~ : 23:59:59] - 장소 : 재택 - 내용 깃헙 로그인 구현 1탄   - template 작성(social_login.html, login.html, signup.html)   - router작성(login/github, login/github/callback)   - github_login(), github_callback() 함수 선언
    - 참고문서 :
      - GITHUB OAUTH : https://docs.github.com/en/developers/apps/authorizing-oauth-apps
      - DJANGO ALLAUTH : https://wikidocs.net/9942
    
    - 느낀점:
      - API끌고와서 사용하는건 구현이 그리 어렵지 않은듯
      - 만약 내가 저런 API만들라고 하면 쉽지 않을듯
    
    17.1 | 17.2 Github Login part 1&2
    - 개발자 : 이혜성
    - 일시 : 2021.02.10.  [ ~ : 23:59:59]
    - 장소 : 재택
    - 내용 깃헙 로그인 구현 2탄
      - `requests`패키지 설치
      - user/admin.pydml  fieldsets 키값 추가, list_diplay 벨류값 추가
      - 소셜 로그인을 위한 상수 추가(users/models.py)
      - 모델의 필드 추가 `login_method`
      - github_callback함수에 추가 로직 작성
    
    - 느낀점:
      - github api는 10분의 유효시간이 있어서 재접속해야한다는데?
      - request모듈 잘쓰면 유용할듯
      - 카카오 소셜 로그인도 비슷한 로직이겠지?
    17.3 Getting Github Profile & Recap
    - 개발자 : 이혜성
    - 일시 : 2021.02.11.  [ ~ : 23:59:59]
    - 장소 : 재택
    - 내용 깃헙 로그인 구현
      -` 커스텀 Exception 구현(GithubException)`
      - `github_callback` 로직 구현
      - `code, error, username` 유무에 따른 분기 로직 작성
      - 커스텀 Exception을 통한 리팩토링
    
    - 참고문서 :
      - None
    - 느낀점:
      - code 리팩토링이 필요함.
      - 내가 봐도 이해하기 쉽지 않음
      - 다음날 되면 까먹겠네. 하지만 한번 짜놓으면 대충 뭔지 감은 오는데...

--- users/admin.py
+++ users/admin.py
@@ -17,6 +17,7 @@ class CustomUserAdmin(UserAdmin):
                                        "language",
                                        "currency",
                                        "superhost",
+                                       "login_method",
                                        ) 
                                 }
         ),
@@ -38,6 +39,8 @@ class CustomUserAdmin(UserAdmin):
                         "is_superuser",
                         "email_verified",
                         "email_secret",
+                        "login_method",
+
     )
 
     
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
+import os
+import requests
+import pprint
 from django.views         import View
 from django.views.generic import FormView
 from django.urls          import reverse_lazy
-from django.shortcuts     import render, redirect, reverse
+from django.shortcuts     import render, redirect, reverse, redirect
 from django.contrib.auth  import authenticate, login, logout
 from users                import forms, models
 
@@ -54,3 +57,61 @@ def complete_verification(request, key):
         # to do : add error message
         pass
     return redirect(reverse('core:home'))
+
+
+def github_login(request):
+    client_id = os.environ.get('GH_ID')
+    redirect_url = 'http://127.0.0.1:8000/users/login/github/callback'
+    return redirect(
+        f"http://github.com/login/oauth/authorize?client_id={client_id}&redirect_url={redirect_url}&scope=read:user"
+    )
+
+class GithubException(Exception):
+    pass
+
+def github_callback(request):
+    try:
+        client_id = os.environ.get("GH_ID")
+        client_secret = os.environ.get("GH_SECRET")
+        code = request.GET.get("code", None)
+        if code is not None:
+            result = requests.post( # requests library를 이용해서 post요청
+                f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+                headers={"Accept":"application/json"},
+            )
+            result_json = result.json()
+            error = result_json.get("error", None)
+            if error is not None: # 깃헙 로그인이 10분이상 지체? 되거나 오류시
+                raise GithubException()
+            else:
+                access_token = result_json.get('access_token')
+                profile_request= requests.get(
+                    "https://api.github.com/user",
+                    headers={
+                        "Authorization":f"token {access_token}",
+                        "Accept": "application/json",
+                    },
+                    )
+                
+                pprint.pprint(profile_request.json())
+                username = profile_json.get('login', None)
+                if username is not None:
+                    name = profile_json.get('name')
+                    email = profile_json.get('email')
+                    bio = profile_json.get('bio')
+                    models.User.objects.get(email=email)
+                    if user is not None:
+                        return redirect(reverse("users:login"))
+                    else:
+                        user = models.User.objects.create(
+                            username=email, first_name=name, bio=bio, email=email)
+                        login(request, user)
+                        return redirect(reverse('core:home'))
+                else:
+                    raise GithubException()
+        else:
+            raise GithubException()
+    except GithubException:
+        return redirect(reverse("users:login"))
+
+        
