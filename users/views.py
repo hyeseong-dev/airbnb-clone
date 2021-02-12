27f67e837339dc2b6bb543f294af376857c79428
이혜성 <hyeseong43@gmail.com>
2021-02-13 05:11:34 +0900

    23.7 Adding Info to The Sessions

--- users/views.py
+++ users/views.py
@@ -1,16 +1,17 @@
 import os
 import requests
-from pprint                        import pprint
-from django.views                  import View
-from django.views.generic          import FormView, DetailView, UpdateView
-from django.urls                   import reverse_lazy
-from django.shortcuts              import render, redirect, reverse, redirect
-from django.contrib                import messages
-from django.contrib.auth           import authenticate, login, logout
-from django.contrib.auth.views     import PasswordChangeView
-from django.core.files.base        import ContentFile
-from django.contrib.messages.views import SuccessMessageMixin
-from users                         import forms, models, mixins
+from pprint                         import pprint
+from django.views                   import View
+from django.views.generic           import FormView, DetailView, UpdateView
+from django.urls                    import reverse_lazy
+from django.shortcuts               import render, redirect, reverse, redirect
+from django.contrib                 import messages
+from django.contrib.auth            import authenticate, login, logout
+from django.contrib.auth.views      import PasswordChangeView
+from django.contrib.auth.decorators import login_required
+from django.core.files.base         import ContentFile
+from django.contrib.messages.views  import SuccessMessageMixin
+from users                          import forms, models, mixins
 
 class LoginView(mixins.LoggedOutOnlyView, FormView):
 
@@ -258,3 +259,11 @@ class UpdatePasswordView(
 
     def get_success_url(self):
         return self.request.user.get_absolute_url()
+
+@login_required
+def switch_hosting(request):
+    try:
+        del request.session['is_hosting']
+    except KeyError:
+        request.session["is_hosting"] = True
+    return redirect(reverse("core:home"))
